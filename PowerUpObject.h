634c4e41d59514cd5d0ade0a703e7044abfee3fc
Jacob Abraham <abrahamjh1@gcc.edu>
2016-04-18 23:07:16 -0400

    merging

---
+++ PowerUpObject.h
@@ -0,0 +1,69 @@
+
+#ifndef PowerUpObject_H
+#define PowerUpObject_H
+
+//#include "d3dUtil.h"
+#include "d3dUtil.h"
+#include "Box.h"
+#include "constants.h"
+#include "ObstacleObject.h"
+#include "GameObject.h"
+
+class PowerUpObject: public GameObject
+{
+public:
+
+	PowerUpObject();
+	~PowerUpObject();
+
+	virtual char type(){return typeOfCar;};
+
+	void init(Box *b, float r, Vector3 pos, Vector3 vel, float sp, float s);
+	void draw();
+	void update(float dt);
+
+	void setPosition (Vector3 pos) {position = pos;}
+	Vector3 getPosition() {return position;}
+	void setVelocity (Vector3 vel) {velocity = vel;}
+	Vector3 getVelocity() {return velocity;}
+	void setSpeed(float s) {speed = s;}
+	float getSpeed() {return speed;}
+	void setRadius(float r) {radius = r; radiusSquared = (scale*r)*(scale*r);}
+	float getRadiusSquare() {return radiusSquared;}
+	float getRadius() {return radius;}
+	Matrix getWorldMatrix() {return world;}
+	void setScale(float s) {scale = s; radiusSquared = (s*radius)*(s*radius);}
+	float getScale() {return scale;}
+	void setActive() {active = true;}
+	void setInActive() {active = false;}
+	bool getActiveState() {return active;}
+	void setMTech(ID3D10EffectTechnique* m){ mTech = m;}
+	bool collided(GameObject *gameObject);
+	bool collided(ObstacleObject *obstacleObject);
+	void setRotationX(float r) {rotX = r;}
+	void setRotationY(float r) {rotY = r;}
+	void setRotationZ(float r) {rotZ = r;}
+	void setAlreadyCollided(bool ac) {alreadyCollided = ac;}
+	bool getAlreadyCollided() {return alreadyCollided;}
+
+	//define setWorldMatrix here
+	void setWorldMatrix(Matrix w) {world = w;}
+
+protected:
+	Box *box; 
+	bool alreadyCollided;
+	Vector3 position;
+	Vector3 velocity;
+	float speed;
+	float radius;
+	float radiusSquared;
+	bool active;
+	Matrix world;
+	float scale;
+	ID3D10EffectTechnique* mTech;
+	float rotX, rotY, rotZ;
+	char typeOfCar;
+};
+
+
+#endif
